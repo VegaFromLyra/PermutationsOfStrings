fae0991d68f46d365cad61a0faf1fad42a928d87
VegaFromLyra <asha.ab@gmail.com>
2014-02-13 12:24:25 -0800

    Given a string, generate all possible permutations of the string

---
+++ PermutationsOfStrings/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationsOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> output = Permute("abc");
+
+            foreach (string str in output)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        static List<string> Permute(string input)
+        {
+            if (input.Length == 1)
+            {
+                List<string> result = new List<string> { input };
+                return result;
+            }
+
+            char last = input[input.Length - 1];
+
+            string rest = input.Substring(0, input.Length - 1);
+
+            List<string> subset = Permute(rest);
+
+            List<string> fullResult = new List<string>();
+
+            foreach (string str in subset)
+            {
+                for (int i = 0; i <= str.Length; i++)
+                {
+                    StringBuilder sb = new StringBuilder(str);
+                    sb.Insert(i, last);
+                    fullResult.Add(sb.ToString());
+                }
+            }
+
+            return fullResult;
+        }
+    }
+}
